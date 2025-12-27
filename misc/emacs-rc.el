f72dae3bfb6eeb4353d440f14be782d1fbd1a524
Far Han Cholakkal <0xfarco@gmail.com>
2025-12-28 01:13:20 +0530

    initial commit

---
+++ misc/emacs-rc.el
@@ -0,0 +1,87 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives
+;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(defvar rc/package-contents-refreshed nil)
+
+(defun rc/package-refresh-contents-once ()
+  (when (not rc/package-contents-refreshed)
+    (setq rc/package-contents-refreshed t)
+    (package-refresh-contents)))
+
+(defun rc/require-one-package (package)
+  (when (not (package-installed-p package))
+    (rc/package-refresh-contents-once)
+    (package-install package)))
+
+(defun rc/require (&rest packages)
+  (dolist (package packages)
+    (rc/require-one-package package)))
+
+(defun rc/require-theme (theme)
+  (let ((theme-package (->> theme
+                            (symbol-name)
+                            (funcall (-flip #'concat) "-theme")
+                            (intern))))
+    (rc/require theme-package)
+    (load-theme theme t)))
+
+(rc/require 'dash)
+(require 'dash)
+
+(rc/require 'dash-functional)
+(require 'dash-functional)
+
+(defun rc/duplicate-line ()
+  "Duplicate current line"
+  (interactive)
+  (let ((column (- (point) (line-beginning-position)))
+        (line (let ((s (thing-at-point 'line t)))
+                (if s (string-remove-suffix "\n" s) ""))))
+    (move-end-of-line 1)
+    (newline)
+    (insert line)
+    (move-beginning-of-line 1)
+    (forward-char column)))
+
+(global-set-key (kbd "C-,") 'rc/duplicate-line)
+
+(defun rc/buffer-file-name ()
+  (if (equal major-mode 'dired-mode)
+      default-directory
+    (buffer-file-name)))
+
+(defun rc/parent-directory (path)
+  (file-name-directory (directory-file-name path)))
+
+;;; Taken from here:
+;;; http://stackoverflow.com/questions/2416655/file-path-to-clipboard-in-emacs
+(defun rc/put-file-name-on-clipboard ()
+  "Put the current file name on the clipboard"
+  (interactive)
+  (let ((filename (rc/buffer-file-name)))
+    (when filename
+      (kill-new filename)
+      (message filename))))
+
+(defun rc/put-buffer-name-on-clipboard ()
+  "Put the current buffer name on the clipboard"
+  (interactive)
+  (kill-new (buffer-name))
+  (message (buffer-name)))
+
+(defun rc/insert-timestamp ()
+  (interactive)
+  (insert (format-time-string "(%Y-%m-%d %H:%M:%S)")))
+
+(global-set-key (kbd "C-x p d") 'rc/insert-timestamp)
+
+(defun rc/rgrep-selected (beg end)
+  (interactive (if (use-region-p)
+                   (list (region-beginning) (region-end))
+                 (list (point-min) (point-min))))
+  (rgrep (buffer-substring-no-properties beg end) "*" (pwd)))
+
+(global-set-key (kbd "C-x p s") 'rc/rgrep-selected)
