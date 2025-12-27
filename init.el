311d04d48a8d122d0963452efc405145ad66956d
Far Han Cholakkal <0xfarco@gmail.com>
2025-12-28 01:39:35 +0530

    remove paredit

--- init.el
+++ init.el
@@ -61,17 +61,6 @@
                          (interactive)
                          (c-toggle-comment-style -1)))
 
-;;; Paredit
-(rc/require 'paredit)
-
-(defun rc/turn-on-paredit ()
-  (interactive)
-  (paredit-mode 1))
-
-(add-hook 'emacs-lisp-mode-hook  'rc/turn-on-paredit)
-(add-hook 'lisp-mode-hook        'rc/turn-on-paredit)
-(add-hook 'common-lisp-mode-hook 'rc/turn-on-paredit)
-
 ;;; Emacs lisp
 (add-hook 'emacs-lisp-mode-hook
           '(lambda ()
